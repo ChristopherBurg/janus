526d68dfa299af8d0c794e5de5cfd520c45b2c10
Christopher Burg <christopherburg@christopherburg.com>
2015-01-26 17:07:39 -0600

    Cleaned the ever living shit out of the code.
    Now that I have some idea of what I’m doing I’ve cleaned up the code
    some. Shit’s more modular now and easier to read.

--- keybase.go
+++ keybase.go
@@ -1,189 +1,232 @@
 package main
 
 import (
-  "bytes"
-  "crypto/hmac"
-  "crypto/sha512"
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "encoding/base64"
-  "encoding/json"
-  "encoding/hex"
-//  "hash"
-  "math"
-  "net/http"
-  "net/url"
-  // "reflect"
-  "golang.org/x/crypto/scrypt"
-  )
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"net/url"
 
+	"golang.org/x/crypto/scrypt"
+)
+
+// Session stores information related to identifying a user to Keybase.io.
+type Session struct {
+	CSRFToken     string
+	SessionCookie string
+}
+
+// User stores information related to a Keybase.io user.
+type User struct {
+	Name string
+	Salt string
+
+	Identity *Session
+}
+
+// The URL used to get a user's salt.
 const GetSaltURL string = "https://keybase.io/_/api/1.0/getsalt.json"
+
+// The URL used to log into Keybase.io.
 const LoginURL string = "https://keybase.io/_/api/1.0/login.json"
 
-type User struct {
-  Name string
-  Salt string
+// Performs an HTTP GET operation against a selected URL with specified
+// parameters. The function will return the response's body and any cookies
+// received since those are the values that Keybase's API rely on.
+func get(URL string, params url.Values) ([]byte, []*http.Cookie, error) {
+
+	getURL, err := url.Parse(GetSaltURL)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	getURL.RawQuery = params.Encode()
+
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", getURL.String(), nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return body, resp.Cookies(), nil
 }
 
-type Session struct {
-  CSRFToken string
-  LoginSession string
+// Performs an HTTP POST operation against a selected URL with the specified
+// JSON values. The function will return the response's body and any cookies
+// received since those are the values that Keybase's API rely on.
+func post(URL string, params map[string]string) ([]byte, []*http.Cookie, error) {
+
+	jsonValues, err := json.Marshal(params)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", URL, bytes.NewBuffer(jsonValues))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return body, resp.Cookies(), nil
 }
 
-func (user *User) GetSalt() (Session, error) {
-  fmt.Println(user.Name)
+// Takes the body returned from Keybase.io's getsalt API call and parses into
+// retrieves the desired data from the returned JSON data.
+func parseSalt(body []byte) (string, string, string, error) {
+	var saltParams map[string]interface{}
+	err := json.Unmarshal(body, &saltParams)
+	if err != nil {
+		return "", "", "", err
+	}
 
-  getSaltUrl, err := url.Parse(GetSaltURL)
-  if err != nil {
-    return Session{}, err
-  }
+	salt := saltParams["salt"].(string)
+	csrfToken := saltParams["csrf_token"].(string)
+	loginSession := saltParams["login_session"].(string)
 
-//  fmt.Println(getSaltUrl)
-
-  params := url.Values{}
-  params.Add("email_or_username", user.Name)
-
-  getSaltUrl.RawQuery = params.Encode()
-
-//  fmt.Println(getSaltUrl)
-
-  resp, err := http.Get(getSaltUrl.String())
-  if err != nil {
-    return Session{}, err
-  }
-  defer resp.Body.Close()
-
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return Session{}, err
-  }
-
-//  fmt.Println(string(body))
-
-  var saltParams map[string]interface{}
-  err = json.Unmarshal(body, &saltParams)
-  if err != nil {
-    return Session{}, err
-  }
-
-//  fmt.Println(saltParams)
-  // fmt.Println("----------RETURNED VALUES----------")
-  // for k, v := range saltParams {
-  //   fmt.Println(k, ":", v, ":", reflect.TypeOf(v))
-  // }
-
-  user.Salt = saltParams["salt"].(string)
-  csrfToken := saltParams["csrf_token"].(string)
-  loginSession := saltParams["login_session"].(string)
-
-  // fmt.Println("CSRF Token:", csrfToken)
-  // fmt.Println("Salt:", user.Salt)
-  // fmt.Println("Login Session:", loginSession)
-//  fmt.Printf("%+v", saltParams)
-
-  session := Session{CSRFToken: csrfToken, LoginSession: loginSession}
-
-  return session, nil
-
+	return salt, csrfToken, loginSession, nil
 }
 
-func (user *User) Login(session Session, passphrase string) (error) {
+func (user *User) getSalt() (string, error) {
+	params := url.Values{}
+	params.Add("email_or_username", user.Name)
 
-  decodedSalt, err := hex.DecodeString(user.Salt)
-  if err != nil {
-    return err
-  }
+	body, _, err := get(GetSaltURL, params)
+	if err != nil {
+		return "", err
+	}
 
-  hash, err := scrypt.Key([]byte(passphrase), decodedSalt, int(math.Pow(2, 15)), 8, 1, 224)
-  if err != nil {
-    return err
-  }
+	salt, csrfToken, loginSession, err := parseSalt(body)
+	if err != nil {
+		return "", err
+	}
 
-  pwh := hash[192:224]
+	user.Salt = salt
+	user.Identity.CSRFToken = csrfToken
 
-  b64Session, err := base64.StdEncoding.DecodeString(session.LoginSession)
-  if err != nil {
-    return err
-  }
+	return loginSession, nil
+}
 
-  fmt.Println("Hash:", pwh)
-  fmt.Println("Hash HEX:", hex.EncodeToString(pwh))
-  fmt.Println("Hash b64:", b64Session)
+// Hash's a user's password use Scrypt.
+func hashPassphrase(salt string, passphrase string) ([]byte, error) {
+	decodedSalt, err := hex.DecodeString(salt)
+	if err != nil {
+		return nil, err
+	}
 
-  hmac := hmac.New(sha512.New, pwh)
-  hmac.Write(b64Session)
+	hash, err := scrypt.Key([]byte(passphrase), decodedSalt, int(math.Pow(2, 15)), 8, 1, 224)
+	if err != nil {
+		return nil, err
+	}
 
-  fmt.Println("HMAC:", hmac.Sum(nil))
-  fmt.Println("HMAC STD B64:", base64.StdEncoding.EncodeToString(hmac.Sum(nil)))
-  fmt.Println("HMAC URL B64:", base64.URLEncoding.EncodeToString(hmac.Sum(nil)))
-  fmt.Println("HMAC HEX:", hex.EncodeToString(hmac.Sum(nil)))
+	pwh := hash[192:224]
 
+	return pwh, nil
+}
 
-  loginValues := map[string]string{"email_or_username" : user.Name,
-                                   "hmac_pwh" : hex.EncodeToString(hmac.Sum(nil)),
-                                   "login_session" : session.LoginSession}
+// Creates an HMAC from the user's hashed password at the current login session.
+func getHMAC(pwh []byte, loginSession string) ([]byte, error) {
+	b64Session, err := base64.StdEncoding.DecodeString(loginSession)
+	if err != nil {
+		return nil, err
+	}
 
-  loginJSON, err := json.Marshal(loginValues)
-  if err != nil {
-    return err
-  }
+	hmacPWH := hmac.New(sha512.New, pwh)
+	hmacPWH.Write(b64Session)
 
-  fmt.Println("JSON Login Data:", string(loginJSON))
+	return hmacPWH.Sum(nil), nil
+}
 
-//  return err
+// Login creates a session for a user.
+func (user *User) Login(passphrase string) error {
+	loginSession, err := user.getSalt()
+	if err != nil {
+		return err
+	}
 
-  // params := url.Values{}
-  // params.Add("email_or_username", user.Name)
-  // params.Add("hmac_pwh", hex.EncodeToString(hmac.Sum(nil)))
-  // params.Add("login_session", session.LoginSession)
+	pwh, err := hashPassphrase(user.Salt, passphrase)
+	if err != nil {
+		return err
+	}
 
-  client := &http.Client{}
-  r, err := http.NewRequest("POST", LoginURL, bytes.NewBuffer(loginJSON))
-//  r, err := http.NewRequest("POST", LoginURL, bytes.NewBufferString(params.Encode()))
-  if err != nil {
-    return err
-  }
+	hmacPWH, err := getHMAC(pwh, loginSession)
+	if err != nil {
+		return nil
+	}
 
-  r.Header.Add("Content-Type", "application/json")
-  fmt.Println(r)
-  fmt.Println(r.Body)
+	loginValues := map[string]string{"email_or_username": user.Name,
+		"hmac_pwh":      hex.EncodeToString(hmacPWH),
+		"login_session": loginSession}
 
-//  return err
+	body, cookies, err := post(LoginURL, loginValues)
+	if err != nil {
+		return err
+	}
 
-  resp, err := client.Do(r)
-  if err != nil {
-    return err
-  }
-  defer resp.Body.Close()
+	for _, cookie := range cookies {
+		if cookie.Name == "session" {
+			user.Identity.SessionCookie = cookie.Value
+		}
+	}
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return err
-  }
+	fmt.Println("Login Body:", string(body))
 
-  fmt.Println(string(body))
-
-  return err
+	return nil
 }
 
 func main() {
-  user := flag.String("user", "", "User name or e-mail address.")
-  passphrase := flag.String("passphrase", "", "Passphrase.")
+	user := flag.String("user", "", "User name or e-mail address.")
+	passphrase := flag.String("passphrase", "", "Passphrase.")
 
-   flag.Parse()
-  // fmt.Println("User: ", *user)
-  // fmt.Println("Passphrase: ", *passphrase)
+	flag.Parse()
 
-  me := User{Name: *user}
+	session := Session{}
+	me := User{Name: *user, Identity: &session}
 
-  session, err := me.GetSalt()
-  if err != nil {
-    panic(err)
-  }
+	err := me.Login(*passphrase)
+	if err != nil {
+		panic(err)
+	}
 
-  err = me.Login(session, *passphrase)
+	// salt, csrfToken, loginSession, err := GetSalt(*user)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-  // fmt.Println("Session:", "CSRFToken:", session.CSRFToken, ":LoginSession:", session.LoginSession)
-  // fmt.Println("Me:", me)
+	//	Login(*user, salt, loginSession, *passphrase)
+
+	fmt.Println("Returned GetSalt values for user", *user, ".")
+	fmt.Println("salt:", me.Salt)
+	fmt.Println("csrfToken:", me.Identity.CSRFToken)
+	// fmt.Println("loginSession:", loginSession)
 }
